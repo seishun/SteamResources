b0b5415b897d90dad78478b28c5803a79d73d37a
seishun <vvnicholas@gmail.com>
2013-11-13 17:21:30 +0200

    Move generate-base.bat files into Generated

--- Protobufs/tf/generate-base.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem the base GC messages are generated from the dota protobufs
-
-echo Building TF2 GC messages
-..\..\Protogen\protogen -s:..\ -i:"tf_gcmessages.proto" -o:"..\..\..\SteamKit2\SteamKit2\Base\Generated\GC\TF2\MsgGC.cs" -t:csharp -ns:"SteamKit2.GC.TF2.Internal"
